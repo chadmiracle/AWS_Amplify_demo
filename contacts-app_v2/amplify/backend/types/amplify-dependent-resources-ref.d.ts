9afa3e4f9845f07715d04710422bbaa552cb9cf6
chad.miracle@alvaria.com <chad.miracle@alvaria.com>
2024-01-22 18:23:53 -0600

    mfa enabled - init commit

--- contacts-app_v2/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ contacts-app_v2/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,10 +1,5 @@
 export type AmplifyDependentResourcesAttributes = {
   "api": {
-    "AdminQueries": {
-      "ApiId": "string",
-      "ApiName": "string",
-      "RootUrl": "string"
-    },
     "contactsappv2": {
       "GraphQLAPIEndpointOutput": "string",
       "GraphQLAPIIdOutput": "string",
@@ -15,21 +10,11 @@ export type AmplifyDependentResourcesAttributes = {
     "contactsappv25dea6035": {
       "AppClientID": "string",
       "AppClientIDWeb": "string",
-      "CreatedSNSRole": "string",
       "IdentityPoolId": "string",
       "IdentityPoolName": "string",
       "UserPoolArn": "string",
       "UserPoolId": "string",
       "UserPoolName": "string"
-    }
-  },
-  "function": {
-    "AdminQueries138ec2b6": {
-      "Arn": "string",
-      "LambdaExecutionRole": "string",
-      "LambdaExecutionRoleArn": "string",
-      "Name": "string",
-      "Region": "string"
     }
   },
   "storage": {
